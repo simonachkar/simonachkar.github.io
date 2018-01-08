4df5ec3ccb493946404c040e1220f9e3c6319a3a
Simon Achkar <alachkarsimon@gmail.com>
2018-01-08 12:41:51 +0200

    initial commit

---
+++ assets/js/scrolling.js
@@ -0,0 +1,58 @@
+(function ($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 54)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function () {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#myNav',
+    offset: 54
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function () {
+    if ($(window).width() < 427) {
+      $("#logo").attr("src", "./assets/imgs/the-lead-logo.png");
+      $("#myNav").addClass("navbar-shrink");
+    }
+    else {
+      if ($("#myNav").offset().top > 100) {
+        $("#logo").attr("src", "./assets/imgs/the-lead-logo.png");
+        $("#myNav").addClass("navbar-shrink");
+      } else {
+        $("#logo").attr("src", "./assets/imgs/the-lead-logo-white.png");
+        $("#myNav").removeClass("navbar-shrink");
+      }
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Hide navbar when modals trigger
+  $('.portfolio-modal').on('show.bs.modal', function (e) {
+    $(".navbar").addClass("d-none");
+  })
+  $('.portfolio-modal').on('hidden.bs.modal', function (e) {
+    $(".navbar").removeClass("d-none");
+  })
+
+})(jQuery); // End of use strict
